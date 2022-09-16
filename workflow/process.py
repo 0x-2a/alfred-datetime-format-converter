b8f29ead22ab6c6c42223bc0fad89bcc2be6c2f7
Joshua Hibschman <1032983+0x-2a@users.noreply.github.com>
2022-09-16 13:41:36 -0400

    Update process.py

--- workflow/process.py
+++ workflow/process.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
+#
+# cd /Users/josh/Library/Application Support/Alfred/Alfred.alfredpreferences/workflows
+#
+
+import calendar
 
 import alfred
-import calendar
+import pytz
 from delorean import utcnow, parse, epoch
+
 
 def process(query_str):
     """ Entry point """
     value = parse_query_value(query_str)
     if value is not None:
         results = alfred_items_for_value(value)
-        xml = alfred.xml(results) # compiles the XML answer
-        alfred.write(xml) # writes the XML back to Alfred
+        xml = alfred.xml(results)  # compiles the XML answer
+        alfred.write(xml)  # writes the XML back to Alfred
+
 
 def parse_query_value(query_str):
     """ Return value for the query string """
@@ -21,12 +28,19 @@ def parse_query_value(query_str):
         else:
             # Parse datetime string or timestamp
             try:
-                d = epoch(float(query_str))
+                f = float(query_str)
+                f_str = "{:.0f}".format(f)
+
+                if len(f_str) > 10:
+                    f = float(f_str[0:10])
+
+                d = epoch(f)
             except ValueError:
                 d = parse(str(query_str))
     except (TypeError, ValueError):
         d = None
     return d
+
 
 def alfred_items_for_value(value):
     """
@@ -43,8 +57,70 @@ def alfred_items_for_value(value):
         title=str(item_value),
         subtitle=u'UTC Timestamp',
         attributes={
-            'uid': alfred.uid(index), 
+            'uid': alfred.uid(index),
             'arg': item_value,
+        },
+        icon='icon.png',
+    ))
+    index += 1
+
+    eastern = pytz.timezone('US/Eastern')
+    # loc_dt = eastern.localize(value.datetime)
+    loc_dt = value.datetime.astimezone(eastern)
+    est_time_str = loc_dt.strftime("%Y-%m-%d %H:%M:%S ET")
+
+    results.append(alfred.Item(
+        title=str(est_time_str),
+        subtitle='',
+        attributes={
+            'uid': alfred.uid(index),
+            'arg': est_time_str,
+        },
+        icon='icon.png',
+    ))
+    index += 1
+
+    results.append(alfred.Item(
+        title=str(value.datetime.strftime("%Y-%m-%d %H:%M:%S UTC")),
+        subtitle='',
+        attributes={
+            'uid': alfred.uid(index),
+            'arg': value.datetime.strftime("%Y-%m-%d %H:%M:%S UTC"),
+        },
+        icon='icon.png',
+    ))
+    index += 1
+
+    est_time_str = loc_dt.strftime("%A %B %-d %Y ET")
+    results.append(alfred.Item(
+        title=str(est_time_str),
+        subtitle='',
+        attributes={
+            'uid': alfred.uid(index),
+            'arg': est_time_str,
+        },
+        icon='icon.png',
+    ))
+    index += 1
+
+    results.append(alfred.Item(
+        title=str(value.datetime.strftime("%A %B %-d %Y UTC")),
+        subtitle='',
+        attributes={
+            'uid': alfred.uid(index),
+            'arg': value.datetime.strftime("%A %B %-d %Y UTC"),
+        },
+        icon='icon.png',
+    ))
+    index += 1
+
+    est_time_str = loc_dt.strftime("%Y-%m-%dT%H:%M:%S%z")
+    results.append(alfred.Item(
+        title=str(est_time_str),
+        subtitle='RFC3339 ET',
+        attributes={
+            'uid': alfred.uid(index),
+            'arg': est_time_str,
         },
         icon='icon.png',
     ))
@@ -53,15 +129,15 @@ def alfred_items_for_value(value):
     # Various formats
     formats = [
         # 1937-01-01 12:00:27
-        ("%Y-%m-%d %H:%M:%S", ''),
+        # ("%Y-%m-%d %H:%M:%S", 'UTC Time'),
         # 19 May 2002 15:21:36
-        ("%d %b %Y %H:%M:%S", ''), 
+        # ("%d %b %Y %H:%M:%S", ''),
         # Sun, 19 May 2002 15:21:36
-        ("%a, %d %b %Y %H:%M:%S", ''), 
+        # ("%a, %d %b %Y %H:%M:%S", ''),
         # 1937-01-01T12:00:27
-        ("%Y-%m-%dT%H:%M:%S", ''),
+        # ("%Y-%m-%dT%H:%M:%S", ''),
         # 1996-12-19T16:39:57-0800
-        ("%Y-%m-%dT%H:%M:%S%z", ''),
+        ("%Y-%m-%dT%H:%M:%S%z", 'RFC3339 UTC'),
     ]
     for format, description in formats:
         item_value = value.datetime.strftime(format)
@@ -69,18 +145,20 @@ def alfred_items_for_value(value):
             title=str(item_value),
             subtitle=description,
             attributes={
-                'uid': alfred.uid(index), 
+                'uid': alfred.uid(index),
                 'arg': item_value,
             },
-        icon='icon.png',
+            icon='icon.png',
         ))
         index += 1
 
     return results
 
+
 if __name__ == "__main__":
-    try:
-        query_str = alfred.args()[0]
-    except IndexError:
-        query_str = None
-    process(query_str)
+    # try:
+    #     query_str = alfred.args()[0]
+    # except IndexError:
+    #     query_str = None
+    # process(query_str)
+    process("now")
